1dbe438a30a688d47ded69a00b1698566002e02e
Jack Cooper <jack@acooper.net>
2021-07-27 14:09:04 +0100

    updatefix

--- main.js
+++ main.js
@@ -3,8 +3,9 @@
 const {app, BrowserWindow, autoUpdater} = require('electron')
 const path = require('path')
 const webServer = require('./web_server');
-const server = "https://mc-client-updater-qtxoapkcg-jackcooper04.vercel.app";
+const server = "https://mc-client-updater-jackcooper04.vercel.app";
 const url =  `${server}/update/${process.platform}/${app.getVersion()}`;
+console.log(url)
 autoUpdater.setFeedURL({url});
 setInterval(() => {
   autoUpdater.checkForUpdates()
