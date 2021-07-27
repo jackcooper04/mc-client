4c155b84c331019c7bdfe005a1866aba7dc4e9bd
Jack Cooper <jack@acooper.net>
2021-07-27 16:51:21 +0100

    transfer

---
+++ buildInstaller.js
@@ -0,0 +1,40 @@
+// ./build_installer.js
+
+// 1. Import Modules
+const { MSICreator } = require('electron-wix-msi');
+const path = require('path');
+
+// 2. Define input and output directory.
+// Important: the directories must be absolute, not relative e.g
+// appDirectory: "C:\\Users\sdkca\Desktop\OurCodeWorld-win32-x64",
+const APP_DIR = path.resolve(__dirname, './mcClient-win32-x64');
+// outputDirectory: "C:\\Users\sdkca\Desktop\windows_installer",
+const OUT_DIR = path.resolve(__dirname, './windows_installer');
+
+// 3. Instantiate the MSICreator
+const msiCreator = new MSICreator({
+    appDirectory: APP_DIR,
+    outputDirectory: OUT_DIR,
+
+    // Configure metadata
+    description: 'Manger for minecraft server',
+    exe: 'mcClient',
+    name: 'Minecraft Server Client',
+    manufacturer: 'Jack Cooper Projects',
+    appIconPath:"C:\\Users\\jack\\Documents\\main-projects\\mc-client\\test.ico",
+    version: '0.15.0',
+    features:{
+      autoUpdate:true
+    },
+    // Configure installer User Interface
+    ui: {
+        chooseDirectory: true
+    },
+});
+
+// 4. Create a .wxs template file
+msiCreator.create().then(function(){
+
+    // Step 5: Compile the template to a .msi file
+    msiCreator.compile();
+});
