de2f11e9d163857cc18cbbcacf63bbb57b5ba9e4
Jack Cooper <jack@acooper.net>
2021-07-29 20:06:27 +0100

    navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 import { ServerCreatorDialogComponent } from '../server-creator-dialog/server-creator-dialog.component';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +10,12 @@ import { ServerCreatorDialogComponent } from '../server-creator-dialog/server-cr
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private matDialog:MatDialog) { }
+  servers:any;
+  serverSub:Subscription;
+  downloadSub:Subscription;
+  isDownloading = false;
+  window = true;
+  constructor(private matDialog:MatDialog, public serverService:ServerService) { }
 
   openDialog(){
     const dialogRef = this.matDialog.open(ServerCreatorDialogComponent, {
@@ -22,6 +28,23 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.serverSub = this.serverService.getServerListener().subscribe(data => {
+
+      this.servers = data.servers;
+      console.log(this.servers)
+    });
+    this.downloadSub = this.serverService.getDownloadStatListener().subscribe(data => {
+      this.isDownloading = data.isDownloading;
+    });
+    this.serverService.getDownloadStatus();
+    this.serverService.getServers();
+  }
+  triggerServer(name:any){
+    this.serverService.activateServer(name,this.window);
+  };
+  changeValue(event:any){
+    this.window = event.checked;
+    console.log(event.checked)
   }
 
 }
